1585e050e78e0c943de55a1ab409beae950cd21f
Dana Robinson <derobins@hdfgroup.org>
2019-05-21 01:41:09 -0700

    Minor tidy of the profile script.

--- shuffle/profile.sh
+++ shuffle/profile.sh
@@ -13,23 +13,22 @@ echo "Times are REAL,USER,SYS in seconds"
 # Loop over shuffle filters
 for shuffle_filter_id in 0 1 315 316
 do
-    # Loop over gzip compression levels
+    # Set the gzip level
     gzip_level=0
-#    for gzip_level in 0 5
-#    do
-        # Run the test program seven times (we'll discard the first)
-        for i in 1 2 3 4 5 6 7
-        do
-            # Delete the file every time
-            rm -rf *.h5
 
-            # Profile the program
-            # The I/O should dominate, so I'm just timing the entire program
-            # instead of instrumenting the I/O calls.
-            time -f "%e,%U,%S" ./shuffle_test_program $shuffle_filter_id $gzip_level
-        done
+    # Run the test program multiple times (we'll discard the first)
+    for i in {1..21}
+    do
+        # Profile the program
+        #
+        # The I/O should dominate, so I'm just timing the entire program
+        # instead of instrumenting the I/O calls.
+        #
+        # Since we're using bash, we need the full path to time to
+        # avoid the built-in.
+        /usr/bin/time -f "%e,%U,%S" ./shuffle_test_program $shuffle_filter_id $gzip_level
+    done
 
-        echo
-        echo
-#    done
+    echo
+    echo
 done
